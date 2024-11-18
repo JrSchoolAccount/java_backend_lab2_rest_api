e014e72d940ba45032063ede508342c58a2c3c8c
JrSchoolAccount <144328779+JrSchoolAccount@users.noreply.github.com>
2024-11-18 16:32:33 +0100

    init

---
+++ src/main/java/org/java/java_backend_lab2_rest_api/app.java
@@ -0,0 +1,13 @@
+package org.java.java_backend_lab2_rest_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class app {
+
+    public static void main(String[] args) {
+        SpringApplication.run(app.class, args);
+    }
+
+}
---
+++ src/test/java/org/java/java_backend_lab2_rest_api/JavaBackendLab2RestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package org.java.java_backend_lab2_rest_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaBackendLab2RestApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
